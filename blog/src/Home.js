648e24a9277d1039ec15306166fd9f48196a0edb
faisoabdirisak <faisoabdirisak@gmail.com>
2022-04-15 12:52:52 +0300

    adds reusung components

--- blog/src/Home.js
+++ blog/src/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
     const [blogs, setBlogs]= useState([
     { title: 'My new website', body: 'lorem ipsum...', author: 'faiso', id: 1 },
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'abdirisak', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'faska', id: 3 }
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'faiso', id: 3 }
     ]);
 
    
@@ -14,6 +14,7 @@ const Home = () => {
     return (  
         <div className="home">
             <BlogList blogs={blogs} title='All Blogs'/>
+              <BlogList blogs={blogs.filter((blog)=> blog.author==='faiso')} title='faiso blocks'/>
         </div>
     );
 }

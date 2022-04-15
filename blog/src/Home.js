c47290e98c17223847fb41b671655ad3e7343d18
faisoabdirisak <faisoabdirisak@gmail.com>
2022-04-15 12:48:04 +0300

    adds props

--- blog/src/Home.js
+++ blog/src/Home.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-
+import  BlogList  from './BlogList';
 
 const Home = () => {
 
@@ -9,30 +9,11 @@ const Home = () => {
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'faska', id: 3 }
     ]);
 
-    const changeContent=()=>{
-        setBlogs([
-    { title: 'My First Blog', body: 'lorem ipsum...', author: 'nanii', id: 1 },
-    { title: 'Second Best Blog!', body: 'lorem ipsum...', author: 'ali', id: 2 },
-    { title: 'Web dev topics', body: 'lorem ipsum...', author: 'shukri', id: 3 }
-        ])
-    }
+   
 
     return (  
         <div className="home">
-          {blogs.map((blog)=>(
-              <div className="blog-preview" key={blog.id}>
-                  <h2>{blog.title}</h2>
-                  <p>Written by {blog.author}</p>
-
-              </div>
-
-          ))}
-          <button style={{
-                    color:"white",
-                    backgroundColor:"#f1356d",
-                    borderRadius:"8px"
-                }} onClick={changeContent}>Change content</button>
-          
+            <BlogList blogs={blogs} title='All Blogs'/>
         </div>
     );
 }

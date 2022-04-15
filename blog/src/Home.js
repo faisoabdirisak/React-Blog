4d6c323141df5424a7911c0d23459e97939476b2
faisoabdirisak <faisoabdirisak@gmail.com>
2022-04-15 12:20:55 +0300

    adds how to render data

--- blog/src/Home.js
+++ blog/src/Home.js
@@ -1,19 +1,37 @@
 import { useState } from 'react';
-const Home = () => {
-    // let name='faiso';
-     const [name, setName] = useState('faiso');
-     const [age, setAge] = useState(26)
 
-    const handleClick=()=>{
-     setName('faska');
-     setAge(24);
+
+const Home = () => {
+
+    const [blogs, setBlogs]= useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'faiso', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'abdirisak', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'faska', id: 3 }
+    ]);
+
+    const changeContent=()=>{
+        setBlogs([
+    { title: 'My First Blog', body: 'lorem ipsum...', author: 'nanii', id: 1 },
+    { title: 'Second Best Blog!', body: 'lorem ipsum...', author: 'ali', id: 2 },
+    { title: 'Web dev topics', body: 'lorem ipsum...', author: 'shukri', id: 3 }
+        ])
     }
-   
+
     return (  
         <div className="home">
-            <h2>Home Page</h2>
-            <p>{name} is {age} years old </p>
-            <button onClick={handleClick}>Click Me</button>
+          {blogs.map((blog)=>(
+              <div className="blog-preview" key={blog.id}>
+                  <h2>{blog.title}</h2>
+                  <p>Written by {blog.author}</p>
+
+              </div>
+
+          ))}
+          <button style={{
+                    color:"white",
+                    backgroundColor:"#f1356d",
+                    borderRadius:"8px"
+                }} onClick={changeContent}>Change content</button>
           
         </div>
     );

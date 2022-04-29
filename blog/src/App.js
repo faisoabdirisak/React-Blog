3eb3285ec823a30187e00b785c0808606f2166f0
faisoabdirisak <faisoabdirisak@gmail.com>
2022-04-29 14:48:51 +0300

    routings

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,18 +1,28 @@
 
-
 import Navbar from './navbar';
 import Home from './Home';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Create from './Create';
 
 function App() { 
   
   return (
-    <div className="App">
-      <Navbar/>
-      <div className="content">
-        <Home/>
-      </div>
+    <Router>
+      <div className="App">
+        <Navbar/>
+        <div className="content">
+         <Switch>
+           <Route exact path='/'>
+             <Home/>
+           </Route>
+           <Route path='/create'>
+             <Create/>
+           </Route>
+         </Switch>
+        </div>
   
     </div>
+    </Router>
   );
 }
 

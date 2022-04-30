b454dd11e5d94c71a78ac273a084b773c9a85815
faisoabdirisak <faisoabdirisak@gmail.com>
2022-04-30 20:50:35 +0300

    forms

--- blog/src/Create.js
+++ blog/src/Create.js
@@ -1,9 +1,40 @@
+import { useState } from "react";
 const Create = () => {
+    const [title, setTitle]=useState('');
+    const [body, setBody]=useState('');
+    const [author, setAuthor]=useState('mario');
     return ( 
         <div className='create'>
             <h2>Add a new Blog</h2>
+            <form>
+                <label>Blog title</label>
+                <input
+                type='text'
+                required 
+                value={body}
+                onChange= {(e) => setBody(e.target.value)}
+                />
+                  <label>Body content</label>
+                <textarea  
+                 required
+                 value={title}
+                 onChange= {(e) => setTitle(e.target.value)}
+                 ></textarea>
+                 <label>Author</label>
+                 <select 
+                 value={author}
+                 onChange={(e)=> setAuthor(e.target.value)}>
+                     <option value='mario'>Mario</option>
+                     <option value='yoshi'>Yoshi</option>
+                 </select>
+                 <button>Add Blog</button>
+                 <p>{title}</p>
+                 <p>{body}</p>
+                 <p>{author}</p>
+               
+            </form>
         </div>
      );
 }
  
-export default Create;
+export default Create;  
